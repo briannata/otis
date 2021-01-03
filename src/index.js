b18329126c7cf0cbceefc20bdb4567eceb89774a
briannata <37271052+briannata@users.noreply.github.com>
2021-01-03 18:30:28 -0500

    setting up website, webcam, and form

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Webcam from "react-webcam";
+ 
+const WebcamComponent = () => <Webcam />;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  class Form extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+        <label>
+          What is your mobile number?<br></br>
+          <input type="text" name="number" class="box" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <br></br>
+        <input type="submit" class="button" value="Submit" />
+      </form>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Webcam />,
+    document.getElementById('left')
+  );
+
+  ReactDOM.render(
+    <Form />,
+    document.getElementById('right')
+  );
