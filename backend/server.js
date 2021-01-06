5cf95a6e6ff0f9e2b8a24bbb7565f2cf7be178ae
2020wmarvil <2020wmarvil@gmail.com>
2021-01-06 01:04:37 -0500

    Add the path module

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -21,7 +22,10 @@ const dataRouter = require('./routes/data');
 app.use('/data', dataRouter);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./build'));
+    app.use(express.static('build'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
 }
 
 app.listen(port, console.log(`Server is starting at ${port}`));
